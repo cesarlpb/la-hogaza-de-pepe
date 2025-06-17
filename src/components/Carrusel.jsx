3b55152282189cfd918f5319c78e407354fb5cb1
César P. <cesarlpb@gmail.com>
2025-06-17 16:47:23 +0200

    añado margin en div

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,7 +1,6 @@
 // src/components/ImageCarousel.jsx
 import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
-import { useRef } from "react";
 
 export default function ImageCarousel({ images }) {
   const [sliderRef] = useKeenSlider({
@@ -10,16 +9,21 @@ export default function ImageCarousel({ images }) {
   });
 
   return (
-    <div ref={sliderRef} className="keen-slider w-full h-64 rounded-xl overflow-hidden">
-      {images.map((src, index) => (
-        <div key={index} className="keen-slider__slide">
-          <img
-            src={src}
-            alt={`Slide ${index + 1}`}
-            className="w-full h-full object-cover"
-          />
-        </div>
-      ))}
+    <div className="w-full flex justify-center">
+      <div ref={sliderRef} className="keen-slider w-full max-w-4xl h-64 rounded-xl overflow-hidden"
+      style={{
+        margin: "0 auto",
+      }}>
+        {images.map((src, index) => (
+          <div key={index} className="keen-slider__slide">
+            <img
+              src={src}
+              alt={`Slide ${index + 1}`}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
